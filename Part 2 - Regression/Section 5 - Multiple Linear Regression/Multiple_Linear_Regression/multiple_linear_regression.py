b84e9593056b230677f416bfacaa2873bf078c9f
Monica G S <monica.seenappa@gmail.com>
2018-07-27 22:48:42 -0700

    Polynomial regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -39,3 +39,48 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
+
+#X_opt = X[:, [0,1,2,3,4,5]]
+#regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#regressor_OLS.summary()
+#
+#X_opt = X[:, [0,1,3,4,5]]
+#regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#regressor_OLS.summary()
+#
+#X_opt = X[:, [0,3,4,5]]
+#regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#regressor_OLS.summary()
+#
+#X_opt = X[:, [0,3,5]]
+#regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#regressor_OLS.summary()
+#
+#X_opt = X[:, [0,3]]
+#regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+#regressor_OLS.summary()
+
+
+# Scripting the above
+
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+
+
