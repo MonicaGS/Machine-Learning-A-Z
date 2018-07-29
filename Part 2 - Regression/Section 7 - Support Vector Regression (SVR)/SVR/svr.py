cd80fc7b527e93b9ea70175d179413c306ad7083
Monica G S <monica.seenappa@gmail.com>
2018-07-28 17:20:21 -0700

    SVR

--- Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
+++ Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/SVR/svr.py
@@ -15,11 +15,11 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 sc_y = StandardScaler()
 X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y = sc_y.fit_transform(y)"""
 
 # Fitting SVR to the dataset
 from sklearn.svm import SVR
